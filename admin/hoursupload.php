c0e5317ad7b8c0819dc2bf8614009556352c6d68
Christopher Wiggins <wiggicr@mail.Broward.edu>
2013-06-01 15:19:17 -0400

    middle

--- admin/hoursupload.php
+++ admin/hoursupload.php
@@ -5,7 +5,7 @@
       
         <h2>Welcome RN to BSN Admin</h2>
 		
-		 <center><img src="Symbol-of-Medicine-300x300.png" style="align:center"/></center>
+		 <img src="Symbol-of-Medicine-300x300.png" style="margin:auto; width:75;"/>
       
       
    <?php
