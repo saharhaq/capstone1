328b82dd0406dce7151eab33294bc8e8b55e873a
Christopher Wiggins <wiggicr@mail.Broward.edu>
2013-06-02 18:14:49 -0400

    background

--- filedelete.php
+++ filedelete.php
@@ -1,11 +1,7 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-  <link rel="stylesheet" type="text/css" href="style.css">
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  <title>RN to BSN Student Profile</title>
-</head>
+<?php
+	// this auto-magically inserts header.html here
+	require('header1.html');  
+?>
 <script>
 function myFunction()
 {
@@ -54,5 +50,9 @@ mysqli_close($dbc);
 
 
 <div align="center"><img src="background.jpg" class="bg"></div>  
-</body>
-</html>
+
+
+   <?php
+	// this auto-magically inserts footer.html here
+	require('footer1.html');
+?>
