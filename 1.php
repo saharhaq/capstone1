a824c0056d6a659a699457a7c4686eec4add47b0
Christopher Wiggins <wiggicr@mail.Broward.edu>
2013-06-02 15:38:29 -0400

    color

--- 1.php
+++ 1.php
@@ -1,12 +1,9 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-  <link rel="stylesheet" type="text/css" href="style.css">
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  <title>Welcome Hours</title>
-</head>
-<body>
+<?php
+	// this auto-magically inserts header.html here
+	require('header.html');  
+?>
+
+
   <h2>Welcome RN to BSN Students</h2>
 
 <?php
@@ -62,6 +59,7 @@ session_start();
  }
 ?>
 
-<div align="center"><img src="background.jpg" class="bg"></div>
-</body>
-</html>
+   <?php
+	// this auto-magically inserts footer.html here
+	require('footer.html');
+?>
