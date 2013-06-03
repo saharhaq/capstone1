c1a3ea91263d8d89537515e1dd984546413b7c85
sahar haque <saharhaque@hotmail.com>
2013-06-03 19:07:12 -0400

    header/footer

--- enterdata.php
+++ enterdata.php
@@ -1,9 +1,8 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-  <link rel="stylesheet" type="text/css" href="style.css">
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<?php
+	// this auto-magically inserts header.html here
+	require('header1.html');  
+?>
+
   <title>Welcome Hours</title>
 </head>
 <body>
@@ -37,8 +36,8 @@
   </p>
 </form>
 
- <a href="logout.php">LOGOUT</a>
 
-<div align="center"><img src="background.jpg" class="bg"></div>
-</body>
-</html>
+   <?php
+	// this auto-magically inserts footer.html here
+	require('footer1.html');
+?>
