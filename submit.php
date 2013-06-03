c1a0bbfea961b815c5f052c1826ec00a5561d0c0
sahar haque <saharhaque@hotmail.com>
2013-06-03 19:14:12 -0400

    break

--- submit.php
+++ submit.php
@@ -32,7 +32,10 @@ $sql = "INSERT INTO `Hours` (id,`StudentID`, `Hours`, `Activity`, `Description`,
 			$result = mysqli_query($dbc, $sql)
 		or die("Could not submit data: " . mysqli_error($dbc));
 
-		echo "<br/>Thank you for your submission<br/>";
+		<br/> echo "<br/>Thank you for your submission<br/>";
+		<br/>
+		<br/>
+		<br/>
 	
 		
 	
