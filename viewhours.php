949b001cd306f3e594a9f227050092ccccaed00b
sahar haque <saharhaque@hotmail.com>
2013-06-03 20:20:10 -0400

    header/footer

--- viewhours.php
+++ viewhours.php
@@ -1,10 +1,8 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  <title></title>
-</head>
+
+<?php
+	// this auto-magically inserts header.html here
+	require('header1.html');  
+?>
 
 <script>
 function myFunction()
@@ -90,13 +88,13 @@ session_start();
 */
 	
 	mysqli_close($dbc);
-
-
 ?>
-
- <a href="logout.php">LOGOUT</a>
 
 </table>
 
-</body>
-</html>
+
+   <?php
+	// this auto-magically inserts footer.html here
+	require('footer1.html');
+?>
+
