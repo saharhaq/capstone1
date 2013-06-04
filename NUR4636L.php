c7f016da6733372f0f8a7fae9ad8376175c11850
sahar haque <saharhaque@hotmail.com>
2013-06-03 20:41:43 -0400

    header

--- NUR4636L.php
+++ NUR4636L.php
@@ -1,12 +1,13 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-  <link rel="stylesheet" type="text/css" href="style.css">
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  <title>Welcome Hours</title>
-</head>
-<body>
+<?php
+	// this auto-magically inserts header.html here
+	require('header1.html');  
+?>
+
+
+
+
+
+
   <h2>Welcome RN to BSN Students</h2>
  
  <?php
@@ -49,6 +50,12 @@
  <a href="1.php">back to courses</a>
  <a href="logout.php">LOGOUT</a>
 <?php } ?>
- <div align="center"><img src="background.jpg" class="bg"></div>
-</body>
-</html>
+
+
+
+
+   <?php
+	// this auto-magically inserts footer.html here
+	require('footer1.html');
+?>
+ 
