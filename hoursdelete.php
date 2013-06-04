f65c452d917669e95757a8c0315d52ce23a6fff1
sahar haque <saharhaque@hotmail.com>
2013-06-03 20:30:45 -0400

    header

--- hoursdelete.php
+++ hoursdelete.php
@@ -1,11 +1,12 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-  <link rel="stylesheet" type="text/css" href="style.css">
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  <title>RN to BSN Student Profile</title>
-</head>
+
+<?php
+	// this auto-magically inserts header.html here
+	require('header1.html');  
+?>
+
+
+
+
 <script>
 function myFunction()
 {
@@ -45,14 +46,14 @@ mysqli_close($dbc);
 ?>
 <a href="viewhours.php">Click here to return</a>
 
- <a href="logout.php">LOGOUT</a>
+ 
 
 
+ 
+ 
 
-
-
-
-
-<div align="center"><img src="background.jpg" class="bg"></div>  
-</body>
-</html>
+<?php } ?>
+   <?php
+	// this auto-magically inserts footer.html here
+	require('footer1.html');
+?>
