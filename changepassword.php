9149f2a6e5f742c8033a423eb67ec90760977809
sahar haque <saharhaque@hotmail.com>
2013-06-05 19:09:23 -0400

    header/footer

--- changepassword.php
+++ changepassword.php
@@ -1,12 +1,15 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-  <link rel="stylesheet" type="text/css" href="style.css">
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+
+<?php
+	// this auto-magically inserts header.html here
+	require('header1.html');  
+?>
+
+
+
+
   <title>Create profile</title>
-</head>
-<body>
+
   <h2>Profile</h2>
 
 <?php
@@ -53,6 +56,9 @@ if (empty($_SESSION['employee_id'])) {
   
 ?>
 
-<div align="center"><img src="background.jpg" class="bg"></div>
-</body>
-</html>
+
+
+   <?php
+	// this auto-magically inserts footer.html here
+	require('footer1.html');
+?>
