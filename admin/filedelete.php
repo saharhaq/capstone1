2db6ba6e86dc595f19cf02913fd49d4fa983e6f1
sahar haque <saharhaque@hotmail.com>
2013-06-06 20:54:40 -0400

    header

--- admin/filedelete.php
+++ admin/filedelete.php
@@ -1,9 +1,12 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-  <link rel="stylesheet" type="text/css" href="style.css">
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<?php
+	// this auto-magically inserts header.html here
+	require('header.html');  
+?>
+
+
+
+
+
   <title>RN to BSN Student Profile</title>
 </head>
 <script>
@@ -46,13 +49,7 @@ mysqli_close($dbc);
 <a href="download.php">Click here to return</a>
 
 
- <a href="logout.php">LOGOUT</a>
-
-
-
-
-
-
-<div align="center"><img src="background.jpg" class="bg"></div>  
-</body>
-</html>
+<?php
+	// this auto-magically inserts header.html here
+	require('header.html');  
+?>
