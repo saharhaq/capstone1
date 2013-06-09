865c29e97355edbfb81bbb0ec104a9b33254e602
Christopher Wiggins <wiggicr@mail.Broward.edu>
2013-06-09 16:01:51 -0400

    header

--- admin/changestudentpass_2.php
+++ admin/changestudentpass_2.php
@@ -1,11 +1,13 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  <title>Admin</title>
-</head>
-<body>
+
+
+<?php
+	// this auto-magically inserts header.html here
+	require('header.html');  
+?>
+
+
+
+
   <h2>Admin</h2>
 
 <?php
@@ -28,5 +30,7 @@
   echo 'Your Profile has been updated succesfully.<br />';
 ?>
 
-</body>
-</html>
+<?php
+	// this auto-magically inserts footer.html here
+	require('footer.html');
+?>
