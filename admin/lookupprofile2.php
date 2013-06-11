753c8d51d204305c4add8eed4e32c71dd700dcf6
sahar haque <saharhaque@hotmail.com>
2013-06-11 16:51:38 -0400

    admin register

--- admin/lookupprofile2.php
+++ admin/lookupprofile2.php
@@ -22,6 +22,20 @@
     or die('Error querying database.');
   $row = mysqli_fetch_array($result);
   mysqli_close($dbc);
+  
+  if ( is_null($row))
+  {  
+  
+    echo 'ID not found';
+	
+	
+ echo '<br /><br /><a href="index.html">Login here</a>';
+  
+  
+  }
+  else{ 
+  
+  
   $first_name = $row['Firstname'];
   $last_name = $row['Lastname'];
   $password = $row['Password'];
@@ -31,9 +45,9 @@
   
 
   echo 'Thank You.<br />';
-  echo 'Your first name is : ' . $first_name . '<br />';
-  echo 'Your last name is : ' . $last_name . '<br />';
-  echo 'Your student ID is : ' . $ProfessorID . '<br />';
+ // echo 'Your first name is : ' . $first_name . '<br />';
+  //echo 'Your last name is : ' . $last_name . '<br />';
+  //echo 'Your student ID is : ' . $ProfessorID . '<br />';
   
   echo '<p> create username and password</p>';
   echo '<form method="post" action="lookupprofile2_1.php">';
