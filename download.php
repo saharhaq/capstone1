4544aab4fbd3fe9afd5dc46948cb31ae427c007a
sahar haque <saharhaque@hotmail.com>
2013-06-11 19:27:43 -0400

    ;

--- download.php
+++ download.php
@@ -9,7 +9,7 @@
     <h2>Welcome RN to BSN Students</h2>
   
   
-   <center style="margin-left:-250px;"><h3><p> <?php echo  $_SESSION['classcode']; ?></p></h3>
+   <center style="margin-left:-250px;"><h3><p> <?php echo  $_SESSION['classcode']; ?></p></h3>;
 
 
 </br>
